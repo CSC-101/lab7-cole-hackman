3a7ae984d7f1eaac90854e44db4230da918482d6
cole-hackman <hackman@calpoly.edu>
2024-11-07 10:16:26 -0800

    Implemented functions for tasks 1, 2, 3
    - Task 1: Created str_to_float(input_str) to convert a string to a float, returning None for invalid conversions. Used exception handling for non-convertible strings.
    -Task 2: Created gather_numbers() to prompt the user for float inputs, converting entries with str_to_float and ignoring invalid inputs. The function reads user inputs until “done” is entered, then returns a list of the floats.
    -Task 3: Created command-line argument processor that converts each argument in sys.argv to a float while ignoring non float entries. It then sums the valid floats and prints the total

--- convert.py
+++ convert.py
@@ -1,1 +1,12 @@
+# Part 1 converts a string to a float
 
+# Parameters:
+# input_str: The string that we want to convert to a float (data type str)
+
+# Returns: The converted float value if conversion is successful, otherwise None if the input string cannot be converted to a float.
+def str_to_float(input_str: str) -> float:
+    try:
+        converted = float(input_str)
+        return converted
+    except ValueError:
+        return None
--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,28 @@
+# Part 2 gathers user-inputted numbers until "done" is entered
+# Parameters: None (takes input from the user directly)
 
+# Returns: A list of floats containing the valid numbers entered by the user.
+# Prompts the user to enter a number, and appends valid numbers to a list.
+# If "done" is entered, stops gathering inputs and returns the list.
+
+def gather_numbers():
+    users_inputs = []
+
+    while True:
+        gathering = input("Enter a number: ")
+        if gathering.lower() == "done":
+            break
+
+        try:
+            number = float(gathering)
+            users_inputs.append(number)
+        except:
+            print("That number is not valid. Please enter a valid number or type done if you are finished.")
+
+    return users_inputs
+
+def main():
+    gather_numbers()
+
+if __name__ == '__main__':
+    main()
